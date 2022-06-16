c011e8dd8e6de9fdbe5614f23489e3fcfb546f9a
JamesFincher <heartandkarma@gmail.com>
2022-06-16 00:46:44 -0700

    got a simple image card with placeholder photo working

--- pages/components/ImageCard.js
+++ pages/components/ImageCard.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import styles from '../../styles/ImageCard.module.scss';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 const Image = () => {
-  return <div>Image</div>;
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant='top' src='https://picsum.photos/200' />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant='primary'>Go somewhere</Button>
+      </Card.Body>
+    </Card>
+  );
 };
 
 export default Image;
