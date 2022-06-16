833a32a13a23752a854a437d3e71c0e553d88132
JamesFincher <heartandkarma@gmail.com>
2022-06-15 23:45:27 -0700

    got some layout going, and some css setup for footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,18 @@
 import '../styles/globals.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Fragment } from 'react';
+import Head from 'next/head';
+import Layout from './components/Layout';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Head>
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+      </Head>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
 export default MyApp;
